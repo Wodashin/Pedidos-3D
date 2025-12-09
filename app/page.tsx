43d863acaca255646a8167015ab96cde7be9aabe
Wodashin <kevinaguilera97@gmail.com>
2025-12-09 21:25:36 +0000

    Migrando proyecto a Next.js

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,8 @@ import {
 } from "lucide-react"
 
 // --- CONFIGURACIÓN DE SUPABASE ---
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || ""
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || ""
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ""
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
